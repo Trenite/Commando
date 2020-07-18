087baecec5a86e2b044a12ee38ed1a34caa7bcef
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2020-07-18 15:40:50 +0200

    translated cmds

--- src/commands/util/eval.js
+++ src/commands/util/eval.js
@@ -1,33 +1,32 @@
-const util = require('util');
-const discord = require('discord.js');
-const tags = require('common-tags');
-const { escapeRegex } = require('../../util');
-const Command = require('../base');
+const util = require("util");
+const discord = require("discord.js");
+const tags = require("common-tags");
+const { escapeRegex } = require("../../util");
+const Command = require("../base");
 
-const nl = '!!NL!!';
-const nlPattern = new RegExp(nl, 'g');
+const nl = "!!NL!!";
+const nlPattern = new RegExp(nl, "g");
 
 module.exports = class EvalCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'eval',
-			group: 'util',
-			memberName: 'eval',
-			description: 'Executes JavaScript code.',
-			details: 'Only the bot owner(s) may use this command.',
-			ownerOnly: true,
-
+			name: "eval",
+			group: "dev",
+			memberName: "eval",
+			description: "Executes JavaScript code.",
+			details: "Only the bot owner(s) may use this command.",
+			devOnly: true,
 			args: [
 				{
-					key: 'script',
-					prompt: 'What code would you like to evaluate?',
-					type: 'string'
-				}
-			]
+					key: "script",
+					prompt: "What code would you like to evaluate?",
+					type: "string",
+				},
+			],
 		});
 
 		this.lastResult = null;
-		Object.defineProperty(this, '_sensitivePattern', { value: null, configurable: true });
+		Object.defineProperty(this, "_sensitivePattern", { value: null, configurable: true });
 	}
 
 	run(msg, args) {
@@ -36,13 +35,13 @@ module.exports = class EvalCommand extends Command {
 		const message = msg;
 		const client = msg.client;
 		const lastResult = this.lastResult;
-		const doReply = val => {
-			if(val instanceof Error) {
+		const doReply = (val) => {
+			if (val instanceof Error) {
 				msg.reply(`Callback error: \`${val}\``);
 			} else {
 				const result = this.makeResultMessages(val, process.hrtime(this.hrStart));
-				if(Array.isArray(result)) {
-					for(const item of result) msg.reply(item);
+				if (Array.isArray(result)) {
+					for (const item of result) msg.reply(item);
 				} else {
 					msg.reply(result);
 				}
@@ -56,55 +55,64 @@ module.exports = class EvalCommand extends Command {
 			const hrStart = process.hrtime();
 			this.lastResult = eval(args.script);
 			hrDiff = process.hrtime(hrStart);
-		} catch(err) {
+		} catch (err) {
 			return msg.reply(`Error while evaluating: \`${err}\``);
 		}
 
 		// Prepare for callback time and respond
 		this.hrStart = process.hrtime();
 		const result = this.makeResultMessages(this.lastResult, hrDiff, args.script);
-		if(Array.isArray(result)) {
-			return result.map(item => msg.reply(item));
+		if (Array.isArray(result)) {
+			return result.map((item) => msg.reply(item));
 		} else {
 			return msg.reply(result);
 		}
 	}
 
 	makeResultMessages(result, hrDiff, input = null) {
-		const inspected = util.inspect(result, { depth: 0 })
-			.replace(nlPattern, '\n')
-			.replace(this.sensitivePattern, '--snip--');
-		const split = inspected.split('\n');
+		const inspected = util
+			.inspect(result, { depth: 0 })
+			.replace(nlPattern, "\n")
+			.replace(this.sensitivePattern, "--snip--");
+		const split = inspected.split("\n");
 		const last = inspected.length - 1;
-		const prependPart = inspected[0] !== '{' && inspected[0] !== '[' && inspected[0] !== "'" ? split[0] : inspected[0];
-		const appendPart = inspected[last] !== '}' && inspected[last] !== ']' && inspected[last] !== "'" ?
-			split[split.length - 1] :
-			inspected[last];
+		const prependPart =
+			inspected[0] !== "{" && inspected[0] !== "[" && inspected[0] !== "'" ? split[0] : inspected[0];
+		const appendPart =
+			inspected[last] !== "}" && inspected[last] !== "]" && inspected[last] !== "'"
+				? split[split.length - 1]
+				: inspected[last];
 		const prepend = `\`\`\`javascript\n${prependPart}\n`;
 		const append = `\n${appendPart}\n\`\`\``;
-		if(input) {
-			return discord.splitMessage(tags.stripIndents`
-				*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
+		if (input) {
+			return discord.splitMessage(
+				tags.stripIndents`
+				*Executed in ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ""}${hrDiff[1] / 1000000}ms.*
 				\`\`\`javascript
 				${inspected}
 				\`\`\`
-			`, { maxLength: 1900, prepend, append });
+			`,
+				{ maxLength: 1900, prepend, append }
+			);
 		} else {
-			return discord.splitMessage(tags.stripIndents`
-				*Callback executed after ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ''}${hrDiff[1] / 1000000}ms.*
+			return discord.splitMessage(
+				tags.stripIndents`
+				*Callback executed after ${hrDiff[0] > 0 ? `${hrDiff[0]}s ` : ""}${hrDiff[1] / 1000000}ms.*
 				\`\`\`javascript
 				${inspected}
 				\`\`\`
-			`, { maxLength: 1900, prepend, append });
+			`,
+				{ maxLength: 1900, prepend, append }
+			);
 		}
 	}
 
 	get sensitivePattern() {
-		if(!this._sensitivePattern) {
+		if (!this._sensitivePattern) {
 			const client = this.client;
-			let pattern = '';
-			if(client.token) pattern += escapeRegex(client.token);
-			Object.defineProperty(this, '_sensitivePattern', { value: new RegExp(pattern, 'gi'), configurable: false });
+			let pattern = "";
+			if (client.token) pattern += escapeRegex(client.token);
+			Object.defineProperty(this, "_sensitivePattern", { value: new RegExp(pattern, "gi"), configurable: false });
 		}
 		return this._sensitivePattern;
 	}
--- src/commands/util/ping.js
+++ src/commands/util/ping.js
@@ -1,28 +1,32 @@
-const { oneLine } = require('common-tags');
-const Command = require('../base');
+const { oneLine } = require("common-tags");
+const Command = require("../base");
 
 module.exports = class PingCommand extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'ping',
-			group: 'util',
-			memberName: 'ping',
-			description: 'Checks the bot\'s ping to the Discord server.',
+			name: "ping",
+			group: "bot",
+			memberName: "ping",
+			description: "Checks the bot's ping to the Discord server.",
+			clientPermissions: ["SEND_MESSAGES"],
 			throttling: {
 				usages: 5,
-				duration: 10
-			}
+				duration: 10,
+			},
 		});
 	}
 
 	async run(msg) {
-		const pingMsg = await msg.reply('Pinging...');
-		return pingMsg.edit(oneLine`
-			${msg.channel.type !== 'dm' ? `${msg.author},` : ''}
+		const pingMsg = await msg.reply("Pinging...");
+		return pingMsg.edit(
+			oneLine`
+			${msg.channel.type !== "dm" ? `${msg.author},` : ""}
 			Pong! The message round-trip took ${
 				(pingMsg.editedTimestamp || pingMsg.createdTimestamp) - (msg.editedTimestamp || msg.createdTimestamp)
 			}ms.
-			${this.client.ws.ping ? `The heartbeat ping is ${Math.round(this.client.ws.ping)}ms.` : ''}
-		`);
+			${this.client.ws.ping ? `The heartbeat ping is ${Math.round(this.client.ws.ping)}ms.` : ""}
+		`,
+			{ title: "Ping" }
+		);
 	}
 };
